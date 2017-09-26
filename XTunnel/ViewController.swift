158c00083efd58c63559ba141bd7e95e94785982
networkextension <b@gmail.com>
2017-09-26 16:16:34 +0800

    Initial Commit

---
+++ XTunnel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XTunnel
+//
+//  Created by yarshure on 2017/9/26.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
